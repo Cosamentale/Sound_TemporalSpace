59fbfd51bc4297fccd1b4c8e2bf96f7f593462fb
AlexV2000 <contact@cosamentale.fr>
2024-01-24 19:31:32 +0200

    OSC_CONTAINER

--- csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
+++ csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a61cc372bb635f456ba0563ab646225e710bd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fecc6af43d25d22728986fc1768437516d8daa41")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
