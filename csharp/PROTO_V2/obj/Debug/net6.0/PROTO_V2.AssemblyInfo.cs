f396ed368c6bcc60bf8f4dc299b146327b2659db
AlexV2000 <contact@cosamentale.fr>
2024-01-27 12:16:07 +0200

    update

--- csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
+++ csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f18d364a552f52e2df025ec8b7dd1660d9bd6d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9858e37193e5e35c70fa30a59e1deb4efcf5aa6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
