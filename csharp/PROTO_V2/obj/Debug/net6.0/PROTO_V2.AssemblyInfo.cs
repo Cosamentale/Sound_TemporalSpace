d0b6bf6f967b967f9a3750f1023b4675ac6b88df
AlexV2000 <contact@cosamentale.fr>
2024-01-29 22:15:18 +0200

    Ok directory process

--- csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
+++ csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9858e37193e5e35c70fa30a59e1deb4efcf5aa6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b026123829a5c33973494c52347294a967e2a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
