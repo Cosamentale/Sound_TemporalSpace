7fc420e306c0057669d2e0fc5f70e97779a5fe01
AlexV2000 <contact@cosamentale.fr>
2024-01-30 19:14:18 +0200

    try to add the shader but don't have time

--- csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
+++ csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8616f5cf9d0b4992a2afbc4b84e07fe8759b904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe9ed961194f6f5e5ee1835d833edb74c660525")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
