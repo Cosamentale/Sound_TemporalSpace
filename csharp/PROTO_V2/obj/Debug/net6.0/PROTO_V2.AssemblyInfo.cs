0280dfb3799c20dd1ea6f621d212d0282ca56d52
AlexV2000 <contact@cosamentale.fr>
2024-02-05 23:24:21 +0200

    add visual effect glowshader

--- csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
+++ csharp/PROTO_V2/obj/Debug/net6.0/PROTO_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5610a3ae7e9db2dbf90c2b1a14dd7550cee72727")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a789bd157ca15390d7b23bdfc1ce7b61451a3a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROTO_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
