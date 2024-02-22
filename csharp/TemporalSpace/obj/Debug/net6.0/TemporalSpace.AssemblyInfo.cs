61ba8d93a72ed14a44f9066e4baaef2aeb8cb4ac
Cosamentale <83541800+Cosamentale@users.noreply.github.com>
2024-02-22 12:16:47 +0200

    V10 change

--- csharp/TemporalSpace/obj/Debug/net6.0/TemporalSpace.AssemblyInfo.cs
+++ csharp/TemporalSpace/obj/Debug/net6.0/TemporalSpace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemporalSpace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023015728bef75f2add28b514a6c6adfdc930c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a6d9b1e5fd217391193c99121ceb0abd0defe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemporalSpace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemporalSpace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
