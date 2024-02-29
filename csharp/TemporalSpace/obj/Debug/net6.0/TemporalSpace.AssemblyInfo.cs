3b06448606fe14b23efeb1ce4daaeea5c8073042
Cosamentale <83541800+Cosamentale@users.noreply.github.com>
2024-02-29 18:09:51 +0200

    comit the patch on official modina git

--- csharp/TemporalSpace/obj/Debug/net6.0/TemporalSpace.AssemblyInfo.cs
+++ csharp/TemporalSpace/obj/Debug/net6.0/TemporalSpace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemporalSpace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe6146f93b9b87a6f090a9c61f84c7f94e19c2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f1d0151b46117dd993add16b7b72d5f39a4918")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemporalSpace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemporalSpace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
